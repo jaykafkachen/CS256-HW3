39919c731a2c7cc56aacf3958dad1be119c539bd
Jay C <jaykafkachen@gmail.com>
2018-04-19 15:55:52 -0700

    Update reverse.h

--- reverse.h
+++ reverse.h
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 /*Write a function that accepts an int  array and the array’s size as arguments. The function should create a copy of the array, except that the element values should be reversed in the copy. The function should return a pointer to the new array. Demonstrate the function in a complete program. */
-
 int* reverse(int arr[], int size)
 {
   int copy[size];
   int *copyptr = copy;
   int *arrptr = arr;
   int *endptr = &arr[size-1];
-  while(arrptr <= endptr)
+  for(int i=size-1;i>=0;i--)
   {
-    *copyptr = *arrptr;
+    *copyptr = *(arrptr+i);
     printf("%d ",*copyptr);
     copyptr++;
-    arrptr++;
   }
   return copyptr;
 }
