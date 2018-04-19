d10d4efae813d99bdda518253183bde44fb24a03
Jay C <jaykafkachen@gmail.com>
2018-04-18 23:27:25 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "geometry.h"
+#include "population.h"
+#include "tictactoe.h"
+#include "reverse.h"
+
+int main()
+{
+  //geometrymenu();
+  printf("\n\n");
+  
+  //popSize();
+  printf("\n\n");
+  
+  //game();
+  printf("\n\n");
+  
+  int arr[] = {4,7,1,2,8,0};
+  printf("Testing reverse() with array = {4,7,1,2,8,0}\nreversing...\n");
+  int *rev = reverse(arr, 6);
+  printf("Reversed: ");
+  arrayPrint(rev, 6);
+  return 0;
+}
